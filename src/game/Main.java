3543e4538929bd2043ece0174991cd202b0b8f3d
Kasper Socha <kasper.socha@jayway.com>
2018-02-06 13:47:05 +0100

    Add initial test for getMoves

--- src/game/Main.java
+++ src/game/Main.java
@@ -3,6 +3,7 @@ package game;
 import ai.AiPlayer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
@@ -17,30 +18,26 @@ public class Main {
     public static void testHumanPlayer() {
         HumanPlayer hp = new HumanPlayer();
         int[][] board = {
-                { -1,  -1,   3,   2,   2,   -1,  -1,  -1,},
-                {-1,  -1,   1,   1,   1,   1,  -5,  -5,},
-                {  -1,   -1,   1,   1,   1,   1,   3,   3,},
-                {  -1,   -1,   1,   1,   1,   1,   1,   2,},
-                {  2,   1,   1,   1,   1,   1,   1,   2,},
-                {  3,   3,   2,   1,   1,   1,   3,   3,},
-                { -5,  -5,   1,   1,   1,   1,  -5,  -5,},
-                { 10,  -5,   3,   2,   2,   3,  -5,  10,}
+                {-1, -1, 3, 2, 2, -1, -1, -1,},
+                {-1, -1, 1, 1, 1, 1, -5, -5,},
+                {-1, -1, 1, 1, 1, 1, 3, 3,},
+                {-1, -1, 1, 1, 1, 1, 1, 2,},
+                {2, 1, 1, 1, 1, 1, 1, 2,},
+                {3, 3, 2, 1, 1, 1, 3, 3,},
+                {-5, -5, 1, 1, 1, 1, -5, -5,},
+                {10, -5, 3, 2, 2, 3, -5, 10,}
         };
         ArrayList<int[]> validMoves = new ArrayList<>();
-        validMoves.add(new int[] {0,0});
-        validMoves.add(new int[] {1,1});
-        validMoves.add(new int[] {7,1});
+        validMoves.add(new int[]{0, 0});
+        validMoves.add(new int[]{1, 1});
+        validMoves.add(new int[]{7, 1});
         hp.getNextMove(board, validMoves);
     }
 
-	public static void main(String[] args) {
-        testGame();
+    public static void main(String[] args) {
+        //testGame();
         //testHumanPlayer();
-
-        ArrayList<int[]> validMoves = new ArrayList<>();
-        validMoves.add(new int[] {0,0});
-        validMoves.add(new int[] {1,1});
-        validMoves.add(new int[] {7,1});
+        //testGetMoves();
 
 //		//board[row][col]
 //		int[][] board = new int[8][8];
@@ -78,5 +75,20 @@ public class Main {
 //			System.out.print("\n");
 //		}
 
-	}
+    }
+
+    private static void testGetMoves() {
+        Game g = new Game();
+        int[][] board = new int[Game.BOARD_SIZE][Game.BOARD_SIZE]; //Default value of elements is zero
+
+        board[3][3] = Game.WHITE;
+        board[4][4] = Game.WHITE;
+        board[3][4] = Game.BLACK;
+        board[4][3] = Game.BLACK;
+
+        ArrayList<int[]> validMoves = g.getMoves(board, Game.BLACK);
+        for (int[] move : validMoves) {
+            System.out.println(Arrays.toString(move));
+        }
+    }
 }
